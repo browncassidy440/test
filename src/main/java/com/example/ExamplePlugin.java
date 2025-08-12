5bfb1dac13161a65cc0b0279de94c3bae1792def
browncassidy440 <browncassidy440@gmail.com>
2025-08-12 02:31:36 -0700

    Update ExamplePlugin.java

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -1,53 +1,113 @@
-package com.example;
+package com.example.stopwatcheq;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
-import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.client.config.ConfigManager;
+import net.runelite.api.events.WorldChanged;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.client.config.ConfigManager;
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
-@Slf4j
 @PluginDescriptor(
-	name = "Example"
+    name = "Stopwatch Equation"
 )
-public class ExamplePlugin extends Plugin
+public class StopwatchEquationPlugin extends Plugin
 {
-	@Inject
-	private Client client;
+    @Inject
+    private Client client;
 
-	@Inject
-	private ExampleConfig config;
+    @Inject
+    private StopwatchEquationConfig config;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
-	}
+    @Inject
+    private OverlayManager overlayManager;
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
-	}
+    @Inject
+    private StopwatchEquationOverlay overlay;
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
+    private long startTime;
+    private long elapsedTime;
+    private boolean running;
 
-	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(ExampleConfig.class);
-	}
+    @Override
+    protected void startUp()
+    {
+        overlayManager.add(overlay);
+    }
+
+    @Override
+    protected void shutDown()
+    {
+        overlayManager.remove(overlay);
+    }
+
+    public void startStopwatch()
+    {
+        startTime = System.currentTimeMillis() - elapsedTime;
+        running = true;
+    }
+
+    public void stopStopwatch()
+    {
+        elapsedTime = System.currentTimeMillis() - startTime;
+        running = false;
+    }
+
+    public void resetStopwatch()
+    {
+        elapsedTime = 0;
+        startTime = 0;
+        running = false;
+    }
+
+    public long getElapsedMillis()
+    {
+        return running ? (System.currentTimeMillis() - startTime) : elapsedTime;
+    }
+
+    public double getEquationResult()
+    {
+        double seconds = getElapsedMillis() / 1000.0;
+        try
+        {
+            Expression e = new ExpressionBuilder(config.equation())
+                    .variables("elapsedSeconds")
+                    .build()
+                    .setVariable("elapsedSeconds", seconds);
+            return e.evaluate();
+        }
+        catch (Exception ex)
+        {
+            return 0; // if equation is invalid
+        }
+    }
+
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged event)
+    {
+        switch (event.getGameState())
+        {
+            case LOGIN_SCREEN:
+                stopStopwatch();
+                break;
+        }
+    }
+
+    @Subscribe
+    public void onWorldChanged(WorldChanged event)
+    {
+        stopStopwatch();
+    }
+
+    @Provides
+    StopwatchEquationConfig provideConfig(ConfigManager configManager)
+    {
+        return configManager.getConfig(StopwatchEquationConfig.class);
+    }
 }
